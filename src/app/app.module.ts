b09a70446281b5ef7ebe50a0bcc12810069aa27e
Mukesh Singh <mukesh.singh@factset.com>
2020-05-16 20:10:15 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeSummaryComponent } from './components/recipe-summary/recipe-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeSummaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'recipes',
+        component: RecipeListComponent
+      },
+      {
+        path: '',
+        redirectTo: '/recipes',
+        pathMatch: 'full'
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
